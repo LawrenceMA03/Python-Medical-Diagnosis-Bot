62fa5cdd22edb7b6c35aed49bac334311c8267ba
LawrenceMA03 <68050503+LawrenceMA03@users.noreply.github.com>
2023-03-12 09:22:07 +0900

    Update diagnosis.py

--- diagnosis.py
+++ diagnosis.py
@@ -6,25 +6,69 @@ welcome_prompt = """Welcome doctor, what would you like to do today?\n
 name_prompt = "What is the patient's name?\n"
 appearance_prompt = "How is the patient's general appearance?\n - 1: Normal appearance\n - 2: Irritable or lethargic\n"
 
+eye_prompt = "How are the patient's eyes?\n - 1: Eyes normal or slightly sunken\n - 2: Eyes very sunken\n"
+skin_prompt = "How is the patient's skin when you pinch it?\n - 1: Normal skin pinch\n - 2: Slow skin pinch\n"
+
+error_message = "Could not save patient and diagnosis due to invalid input"
+
+severe_dehydration = "Severe dehydration"
+some_dehydration = "Some dehydration"
+no_dehydration = "No dehydration"
+
+
+patients_and_diagnoses = [
+    "Mike - Severe dehydration",
+    "Sally - No dehydration",
+    "Kate - Some dehydration"
+]
+
 def list_patients():
-    print("Listing patients and diagnoses")
+    for patient in patients_and_diagnoses:
+        print(patient)
 
-# Try calling the 2 functions below according to the appearance_prompt input!
-def assess_skin():
-    print("Assessing skin")
+def save_new_diagnosis(name, diagnosis):
+    if name == "" or diagnosis == "":
+        print(error_message)
+        return
+    final_diagnosis = name + " - " + diagnosis
+    patients_and_diagnoses.append(final_diagnosis)
+    print("Final diagnosis:", final_diagnosis, "\n")
 
-def assess_eyes():
-    print("Assessing eyes")
+def assess_skin(skin):
+    if skin == "1":
+        return some_dehydration
+    elif skin == "2":
+        return severe_dehydration
+    else:
+        return ""
+
+def assess_eyes(eyes):
+    if eyes == "1":
+        return no_dehydration
+    elif eyes == "2":
+        return severe_dehydration
+    else:
+        return ""
+
+def assess_appearance():
+    appearance = input(appearance_prompt)
+    if appearance == "1":
+        eyes = input(eye_prompt)
+        return assess_eyes(eyes)
+    elif appearance == "2":
+        skin = input(skin_prompt)
+        return assess_skin(skin)
+    else:
+        return ""
 
 
 def start_new_diagnosis():
-    # Asks user to enter the patient's name
     name = input(name_prompt)
-    # Asks user to enter how the general appearance of the patient is
-    appearance = input(appearance_prompt)
+    diagnosis = assess_appearance()
+    save_new_diagnosis(name, diagnosis)
+
 
 def main():
-    # Executes the code within the while loop until user presses 'q'
     while(True):
         selection = input(welcome_prompt)
         if selection == "1":
@@ -32,7 +76,54 @@ def main():
         elif selection == "2":
             start_new_diagnosis()
         elif selection == "q":
-            # Exits the program
             return
 
-main()
+#main()
+
+# Assessing all if-then-else cases for both skin and eyes
+def test_assess_skin():
+    print(assess_skin("1") == some_dehydration)
+    print(assess_skin("2") == severe_dehydration)
+    print(assess_skin("3") == "")
+
+def test_assess_eyes():
+    print(assess_eyes("1") == no_dehydration)
+    print(assess_eyes("2") == severe_dehydration)
+    print(assess_eyes("3") == "")
+
+def test_assess_appearance():
+    print(assess_appearance())
+
+# Assessing all possible cases of entries for name and diagnosis
+def test_save_new_diagnosis():
+    save_new_diagnosis("", "")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration"
+    ])
+    save_new_diagnosis("Nimish", "")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration"
+    ])
+    save_new_diagnosis("", "No dehydration")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration"
+    ])
+    save_new_diagnosis("Nimish", "Some dehydration")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration",
+        "Nimish - Some dehydration"
+    ])
+
+
+test_assess_skin()
+test_assess_eyes()
+#test_assess_appearance()
+test_save_new_diagnosis()
